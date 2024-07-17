c8804f9209142c4817296be3e9f706a069b535e7
今天吃没吃123 <13580127+have-you-eaten-today-123@user.noreply.gitee.com>
2024-07-17 14:07:08 +0800

    进度条子组件

---
+++ src/components/UploadProgress.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineOptions({name:'UploadProgress'})
+defineProps({
+  progressPercent:Number
+})
+</script>
+
+<template>
+  <el-progress
+    color="blue"
+    :text-inside="true"
+    :stroke-width="15"
+    :percentage="progressPercent"
+    >
+  </el-progress>
+</template>
+
+<style>
+</style>
