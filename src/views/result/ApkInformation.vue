b22f71f5c277da136e2708fdcc8361e104b94000
今天吃没吃123 <13580127+have-you-eaten-today-123@user.noreply.gitee.com>
2024-07-17 14:06:33 +0800

    文件报告页面

---
+++ src/views/result/ApkInformation.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {ref} from 'vue'
+defineOptions({name:'ApkInformation'})
+const labelSize=ref({
+  fontSize:'20px'
+})
+</script>
+
+
+<template>
+  <page-contain title="报告分析">
+    <el-descriptions title="Apk包名" column="2" class="label">
+      <el-descriptions-item labelClassName="my-label">
+        <template #label >
+          文件信息
+        </template>
+      </el-descriptions-item>
+      <el-descriptions-item label="APK信息" labelClassName="my-label"></el-descriptions-item>
+      <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
+      <el-descriptions-item label="备注">
+        <el-tag size="small">学校</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+    </el-descriptions>
+  </page-contain>
+</template>
+
+<style scoped>
+.label{
+
+}
+.label  ::v-deep .my-label{
+    font-size: 20px;
+}
+.my-label{
+  font-size: 30px!important;
+}
+</style>
